908041e133432d27c54b1f0c600b8d793c3b61f1
Noga25 <nogale40@gmail.com>
2024-02-14 02:47:17 +0200

    Add project files.

---
+++ ObservableLimitedList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DlegentsHomework
+{
+    class ObservableLimitedList
+    {
+        List<string> list = new List<string>() { };
+        public event Action<string> ListChanged;
+        private Predicate<string> _Predicate;
+
+        public bool ContainsS(string userName)
+        {
+            return userName.Contains("s");
+        }
+
+        public void Observable_Limited_List(Predicate<string> predicate)
+        {
+            _Predicate = predicate;
+        }
+
+        public bool TryAdd(string userName)
+        {
+            if (ContainsS(userName))
+            {
+                list.Add(userName);
+                OnListChanged(userName);
+                return true;
+            }
+
+            return false;
+        }
+
+        private void OnListChanged(string message)
+        {
+            ListChanged?.Invoke(message);
+        }
+
+        public void ListChangedHandler(string userName)
+        {
+            Console.WriteLine($"List changed. Item added: {userName}");
+        }
+
+        public void PrintAll()
+        {
+            foreach (var iteams in list)
+            {
+                Console.WriteLine(iteams);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+namespace DlegentsHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ObservableLimitedList A_list = new ObservableLimitedList();
+
+            Predicate<string> predicate = A_list.ContainsS;
+
+            ObservableLimitedList observableList = new ObservableLimitedList();
+
+            observableList.ListChanged += A_list.ListChangedHandler;
+
+            int sItemsCount = 0;
+            for (int i = 0; i <= 10; i++)
+            {
+                string newItem = "Item" + i;
+                if (A_list.ContainsS(newItem))
+                {
+                    sItemsCount++;
+                    A_list.TryAdd(newItem);
+                }
+
+                if (sItemsCount == 3)
+                {
+                    Console.WriteLine("All items added:");
+                    A_list.PrintAll();
+                }
+                
+                else
+                {
+                    Console.WriteLine("The words don't meet the requirements");
+                }
+            }
+        }
+    }
+}
